3c3f0f811ddf04ec1c70a683e086242d5084bc14
EC2 Default User <ec2-user@ip-172-31-94-98.ec2.internal>
2026-07-14 15:33:46 +0000

    Fix merge conflict markers

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -14,19 +12,3 @@ def health():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-=======
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Infrastructure Automation with Monitoring & Self-Healing"
-
-@app.route('/health')
-def health():
-    return "Application is Healthy"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
->>>>>>> c7c1cf4d96aa3aa57806fae83e335deeb6c244bb
